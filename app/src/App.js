3f6c03b56c4216a9ab88f55fc452980148911ef1
oxhelium <quentincrto@gmail.com>
2023-01-21 23:38:07 +0100

    updated app

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { ethers } from "ethers";
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [currentAccount, setCurrentAccount] = useState("");
+  const [provider, setProvider] = useState(null);
+  const connectWallet = async () => {
+    try {
+      if (typeof window.ethereum != "undefined") {
+        //console.log("There is metamask");
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        const tprovider = new ethers.providers.Web3Provider(window.ethereum);
+        setProvider(tprovider);
+        setCurrentAccount(accounts[0]);
+      } else {
+        console.log("You don't have metamask");
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    connectWallet();
+  }, [currentAccount]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+        <div className="header">
+          <p className="logo">WEB3LOTTO</p>
+          {currentAccount === "" ? (
+            <button className="deposit" onClick={() => connectWallet()}>
+              Connect Wallet
+            </button>
+          ) : (
+            <p>...{currentAccount.substring(currentAccount.length - 7)}</p>
+          )}
+        </div>
+        <div className="text">
+          Web3 Lottery is a suer centred lottery, deposit any amount of Eth into
+          the contract, wait for the timestamp to end , and you might win a
+          share of the pool, get your exact Usd deposited equivalent in Eth .
+        </div>
+      
     </div>
   );
 }
